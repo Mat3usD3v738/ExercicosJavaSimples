a09cfbca22645be3a1c035d6b992cd55720b7031
Mateus <mateusoliveiradevk2507@gmail.com>
2024-11-02 19:11:43 -0300

    primeiro commit

---
+++ ContarVogais.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ContarVogais {
+
+/*Chat gpt */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite uma frase: ");
+        System.out.println("\n");
+        String frase = scanner.nextLine().toLowerCase();
+        
+        int contador = 0;
+        for (char c : frase.toCharArray()) {
+            if ("aeiouAEIOUÃÕíìÍÌEÈÉéè".indexOf(c) != -1) {
+                contador++;
+            }
+        }
+        System.out.println("Número de vogais: " + contador);
+        scanner.close();
+    }
+}
+
